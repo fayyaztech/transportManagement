fbada4f033069af7afb5fb7e9c0245589b80abab
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-07-19 16:39:53 +0530

    New function custom_log added

--- application/helpers/genral_helper.php
+++ application/helpers/genral_helper.php
@@ -36,3 +36,9 @@ function alphabet_array()
 	return array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
 }
 
+function custom_log($text)
+{
+	echo date("Y-m-d H:i:s")." ".$text;
+	echo "</br>";
+}
+
