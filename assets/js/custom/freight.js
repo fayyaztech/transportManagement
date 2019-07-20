0c3236e96cc06b5a144c008b9c789b877e48a489
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-07-20 16:21:37 +0530

    no need freight js any more

--- assets/js/custom/freight.js
+++ assets/js/custom/freight.js
@@ -1,17 +1,1 @@
-var url;
-jQuery(document).ready(function($) {
-	url = $("#url").val();
-	console.log(url);
-});
-
-$("#submit_btn").click(function(event) {
-	var consignor_id = $("#consignor").val();
-	var file_option = $("#file_option").val();
-
-	console.log(url+"freight/download_excel_sample?consignor_id="+consignor_id+"&file_option="+file_option);
-
-	$("#download_link").attr("href",url+"freight/download_excel_sample?consignor_id="+consignor_id+"&file_option="+file_option);
-	$("#download_link").trigger('click');
-
-});
 
