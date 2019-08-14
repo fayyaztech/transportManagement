99cb8e745bb24b8e1467a562fbbe71930fcb94fd
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-08-14 14:03:04 +0530

    form separated form master

---
+++ application/views/vehicle/vehicle_form.php
@@ -0,0 +1,175 @@
+<div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header bg-info">
+                                <div class="modal-title">Vehicle</div>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                <span class="sr-only">Close</span>
+                                </button>
+                            </div>
+                            <form id="submit" method="post" action="javascript:void(0);">
+                                <div class="modal-body">
+                                    <div class="card mx-auto">
+                                        
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Owner Name</label>
+                                                        <input type="text" id="owner_name" name="owner_name" class="form-control" placeholder="Owner Name" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="passing no">Engine Number</label>
+                                                        <input type="text" id="vehicle_engine_no" name="vehicle_engine_no" class="form-control" placeholder="Engine Number" required="required">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Purchase/Dilivery date</label>
+                                                        <input type="date" id="purchase_date" name="purchase_date" class="form-control" placeholder="Dilivery date" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Vehicle class</label>
+                                                        <input type="text" id="vehicle_class" name="vehicle_class" class="form-control" placeholder="Vehicle Class" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Body Type</label>
+                                                        <input type="text" id="body_type" name="body_type" class="form-control" placeholder="Body Type" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Maker</label>
+                                                        <input type="text" id="maker" name="maker" class="form-control" placeholder="Maker" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Maker Model</label>
+                                                        <input type="text" id="maker_model" name="maker_model" class="form-control" placeholder="Maker Model" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Unladen Weight(kg.)</label>
+                                                        <input type="number" id="unladen_wt" name="unladen_wt" class="form-control" placeholder="Unladen Weight" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Laden Weight(kg.)</label>
+                                                        <input type="number" id="laden_wt" name="laden_wt" class="form-control" placeholder="Laden Weight" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Seating Capacity</label>
+                                                        <input type="number" id="seating_cap" name="seating_cap" class="form-control" placeholder="Seating Capacity" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Manufacture Year</label>
+                                                        <input type="date" id="manufacture" name="manufacture" class="form-control" placeholder="Manufacture year" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="passing no">Chassis Number</label>
+                                                        <input type="text" id="vehicle_chassis_no" name="vehicle_chassis_no" class="form-control" placeholder="Chassis Number" required="required">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="passing no">Registration Date</label>
+                                                        <input type="date" id="reg_date" name="reg_date" class="form-control" placeholder="Registration date" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">Vehicle Number</label>
+                                                        <input type="text" id="vehicle_number" name="vehicle_number" class="form-control" placeholder="Vehicle Name" required="required" autofocus="autofocus">
+                                                        
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="purchase year">Start Km</label>
+                                                        <input type="number" name="vehicle_current_reading" class="form-control" placeholder="Start Km" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="passing no">Vehicle type</label>
+                                                        <input type="text" id="vehicle_type" name="vehicle_type" class="form-control" placeholder="Vehicle type">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            
+                                            <div class="row">
+                                                
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="passing no">Vehicle Category</label>
+                                                        <input type="text" id="vehicle_category" name="vehicle_category" class="form-control" placeholder="Vehicle Category" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="passing no">Expected Average</label>
+                                                        <input type="number" id="vehicle_expected_average" name="vehicle_expected_average" class="form-control" placeholder="Expected Average" required="required">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="purchase year">Number Of Tyres</label>
+                                                        <input type="number" id="vehicle_tyres" name="vehicle_tyres" class="form-control" placeholder="Number of Tyres" required="required">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-info" id="submit">Add Vehicle</button>
+                                    <input type="reset" class="btn btn-info" placeholder="Number of Tyres" required="required">
+                                    
+                                </div>
+                            </form>
+                            </div><!-- /.modal-content -->
+                            </div><!-- /.modal-dialog -->
