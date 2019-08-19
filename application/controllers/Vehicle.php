deb6afe929a50a3ca65a1e554dba0ada5bfe6f91
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-08-19 09:06:05 +0530

    fetch vehicle info deleted no need any more The same function will work for updating and adding new

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -11,8 +11,15 @@ class Vehicle extends CI_Controller {
 
 	public function vehicle_form()
 	{
-		// both add and update operation perform in single file 
-		$this->load->view('vehicle/vehicle_form');
+		// both add and update operation perform in single file
+		$vehicle_data = [];
+		$vehicle_id = $this->input->get('vehicle_id');
+		if ($vehicle_id != NULL) {
+			if (!empty($data = $this->vehicle_model->fetch_single_vehicle_info($vehicle_id))) {
+				$vehicle_data = $data;
+			}
+		}
+		$this->load->view('vehicle/vehicle_form',['vehicle_data'=>$vehicle_data]);
 	}
 
 		public function add_vehicle()
@@ -20,7 +27,7 @@ class Vehicle extends CI_Controller {
 		$data=$this->input->post();
 		$config= array(
 		array('field' =>'vehicle_number' ,'rules'=>'required|min_length[7]|max_length[10]'),
-		array('field' =>'purchase_date' ,'rules'=>'required|numeric'),
+		array('field' =>'vehicle_purchase_date' ,'rules'=>'required|date'),
 		array('field' =>'vehicle_current_reading' ,'rules'=>'required|numeric'),
 		array('field' =>'vehicle_chassis_no' ,'rules'=>'required'),
 		array('field' =>'vehicle_engine_no' ,'rules'=>'required'),
@@ -90,101 +97,6 @@ class Vehicle extends CI_Controller {
 
 		}
 
-	}
-
-	public function fetch_vehicle_info()
-	{
-		$vehicle_id= $this->input->get('vehicle_id');
-		
-		$data=$this->vehicle_model->fetch_single_vehicle_info($vehicle_id);
-		foreach ($data as $value) {
-		echo 
-		'<div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-info">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    <span class="sr-only">Close</span>
-                    </button>
-                </div>
-                
-                <form id="update_form" method="post" action="javascript:void(0);">
-                <input value="'.$value->vehicle_id.'" type="hidden" class="form-control" required="required" name="vehicle_id">
-                    <div class="modal-body">
-                        <div class="card mx-auto">
-                            <div class="card-header  text-lg-center">Add Vehicle</div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="firstName">Vehicle Name</label>
-                                            <input type="text" id="vehicle_number" name="vehicle_number" class="form-control" placeholder="Vehicle Name" required="required" autofocus="autofocus" value='.$value->vehicle_number.'>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="lastName">Maximum Capacity</label>
-                                            <select id="input" name="vehicle_load_capacity" class="form-control" required="required">
-                                                <option value='.$value->vehicle_load_capacity.'>'.$value->vehicle_load_capacity.'</option>
-                                                <option value="10">10</option>
-                                            </select>
-                                           <!--  <input type="text" id="vehicle_load_capacity" name="vehicle_load_capacity" class="form-control" placeholder="Maximum Capacity" required="required"> -->
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="purchase year">Purchase Year</label>
-                                            <input type="text" id="vehicle_purchase_year" name="vehicle_purchase_year" class="form-control" placeholder="Purchase year" required="required" value='.$value->vehicle_purchase_year.'>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="purchase year">Start Km</label>
-                                            <input type="text" name="vehicle_current_reading" class="form-control" placeholder="Start Km" required="required" value='.$value->vehicle_current_reading.'>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                   <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="passing no">Chassis Number</label>
-                                            <input type="text" id="vehicle_chassis_no" name="vehicle_chassis_no" class="form-control" placeholder="Chassis Number" required="required" value='.$value->vehicle_chassis_no.'>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="passing no">Engine Number</label>
-                                            <input type="text" id="vehicle_engine_no" name="vehicle_engine_no" class="form-control" placeholder="Engine Number" required="required" value='.$value->vehicle_engine_no.'> 
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="passing no">Expected Average</label>
-                                            <input type="text" id="vehicle_expected_average" name="vehicle_expected_average" class="form-control" placeholder="Expected Average" required="required" value='.$value->vehicle_expected_average.'>
-                                        </div>
-                                    </div>
-                                     <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="purchase year">Number Of Tyres</label>
-                                            <input type="text" id="vehicle_tyres" name="vehicle_tyres" class="form-control" placeholder="Number of Tyres" required="required" value='.$value->vehicle_tyres.'>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-info" id="update_vehicle">Update Vehicle</button>
-                        <input type="reset" class="btn btn-info" placeholder="Number of Tyres" required="required" >
-                    </div>
-                </form>
-                </div>
-                </div>';
-            }
 	}
 
 	public function __construct()
