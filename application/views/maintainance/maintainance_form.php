9ee01a4d9b2d217bec81802d0a813d1ae257b372
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-08-22 00:40:24 +0530

    add view edit ready

--- application/views/maintainance/maintainance_form.php
+++ application/views/maintainance/maintainance_form.php
@@ -1,4 +1,4 @@
-<form id="maintainance_form" method="post" action="javascript:void(0);">
+<form id="maintainance_form" action="javascript:void(0);">
     <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <h4 class="modal-title">Maintanance Form</h4>
@@ -17,77 +17,73 @@
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Type</label>
-                    <select class="form-control" name="mnt_type" id="mnt_type">
-                        <option value="1">Tyre</option>
-                        <option value="2">Oil</option>
-                        <option value="3">Battry</option>
-                        <option value="4">Body Work</option>
-                        <option value="5">Engin Work</option>
-                        <option value="6">Misc. Repair</option>                        
+                    <select class="form-control" name="mnt_type" id="mnt_type" <?php if($action == "view")echo "disabled"; ?>>
+                    <?php for($i = 0; $i < count($mnt_types);$i++){?>
+                        <option value="<?php echo $mnt_types[$i]['mnt_type_id'];?>" <?php if(isset($mnt_record) && ($mnt_record['mnt_type'] == $mnt_types[$i]['mnt_type_id']))echo "selected"; ?>><?php echo $mnt_types[$i]['mnt_type_name']?></option>
+                    <?php } ?>                      
                     </select>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Date</label>
-                    <input type="date" name="mnt_date" class="form-control" required="required">
+                    <input type="date" name="mnt_date" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_date'].'"'; if($action == "view")echo "disabled";?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Brand Name</label>
-                    <input type="text" id="mnt_name" name="mnt_name" class="form-control" required="required">
+                    <input type="text" id="mnt_name" name="mnt_name" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_name'].'"'; if($action == "view")echo "disabled";?>>
                 </div>
             </div>
 
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Bill No.</label>
-                    <input type="text" name="mnt_bill_no" class="form-control" value=""
-                        required="required">
+                    <input type="text" name="mnt_bill_no" class="form-control" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_bill_no'].'"'; if($action == "view")echo "disabled";?> required="required">
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Shop Name</label>
-                    <input type="text" name="mnt_shop_name" class="form-control" required="required">
+                    <input type="text" name="mnt_shop_name" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_shop_name'].'"'; if($action == "view")echo "disabled";?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Amount</label>
-                    <input type="text" name="mnt_amount" class="form-control" required="required">
+                    <input type="text" name="mnt_amount" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_amount'].'"'; if($action == "view")echo "disabled";?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Searial No.</label>
-                    <input type="text" name="mnt_serial_no" class="form-control" required="required">
+                    <input type="text" name="mnt_serial_no" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_serial_no'].'"'; if($action == "view")echo "disabled";?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Warranty (in Months)</label>
-                    <input type="text" name="mnt_warranty" class="form-control" required="required">
+                    <input type="text" name="mnt_warranty" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_warranty'].'"'; if($action == "view")echo "disabled";?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Total Run</label>
-                    <input type="text" name="mnt_total_ran" class="form-control" required="required">
+                    <input type="text" name="mnt_total_ran" class="form-control" required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_total_ran'].'"'; if($action == "view")echo "disabled"; ?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Expected Run</label>
-                    <input type="text" id="vm_expected_km" name="mnt_expected_run" class="form-control"
-                        required="required">
+                    <input type="text"  name="mnt_expected_run" class="form-control"
+                        required="required" <?php if(isset($mnt_record))echo 'value="'.$mnt_record['mnt_expected_run'].'"'; if($action == "view")echo "disabled"; ?>>
                 </div>
             </div>
             <div class="col-md-4 col-md-offset-1">
                 <div class="form-group">
                     <label for="">Note</label>
-                    <textarea id="vm_note" name="mnt_note" class="form-control"></textarea>
+                    <textarea id="vm_note" name="mnt_note" class="form-control" <?php if($action == "view")echo "disabled"; ?>><?php if(isset($mnt_record))echo $mnt_record['mnt_note']; ?></textarea>
                 </div>
             </div>
         </div>
@@ -95,6 +91,14 @@
     <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary" <?php if($action == "view")echo "disabled"; ?>>
+        <?php
+            if($action == "new"){
+                echo "save Changes";
+            }elseif($action == "edit" || $action == "view"){
+                echo "update data";
+            }
+        ?>
+        </button>
     </div>
 </form>
