7f42ee402fb7fdbfa3223ebf4bd7180a591aba38
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-08-26 23:49:49 +0530

    Route model done

--- application/controllers/Route.php
+++ application/controllers/Route.php
@@ -1,77 +1,67 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Route extends CI_Controller {
+class Route extends CI_Controller
+{
 
-	public function index()
-	{
+    public function index()
+    {
+        $data['route_info'] = $this->routes_model->fetch_route();
+        template($this, 'route', $data);
+    }
 
-		$count = 1;	
-		$route_info = $this->routes_model->fetch_route();
-		foreach ($route_info as  $value) {
-			echo'
-				<tr>
-					<td>'.$count++.'</td>
-					<td>'.$value->route_origin.'</td>
-					<td>'.$value->route_destination.'</td>
-					<td>'.$value->route_distance.' Km</td>
-					
-					<td>
-                     <a class="btn btn-danger fa fa-trash" id="delete_route_btn" value="'.$value->route_id.'"></a>
-                    </td>
-				</tr>
-			';
-		}
-	}
+    public function delete_route()
+    {
+        $route = $this->input->get('route_id');
+        if ($this->routes_model->delete_route($route)) {
+            echo 1;
+        } else {
+            echo 0;
+        }
+    }
 
+    public function add_route_form()
+    {
+        $data = [];
+        $this->load->view('route/add_route_form', $data);
+    }
 
-	public function delete_route()
-	{
-		$route = $this->input->get('route_id');
-		if ($this->routes_model->delete_route($route)) {
-			echo 1;
-		}else{
-			echo 0;
-		}
-	}
+    public function add_route()
+    {
+        $post = $this->input->post();
 
-	
-	public function add_route(){
-		$post = $this->input->post();
-		
-		$validation = array(
-			array('field'=>"route_origin", 'rules'=>'required'),
-			array('field'=>"route_destination", 'rules'=>'required'),
-			array('field'=>"route_distance", 'rules'=>'required'),
-			
+        $validation = array(
+            array('field' => "route_origin", 'rules' => 'required'),
+            array('field' => "route_destination", 'rules' => 'required'),
+            array('field' => "route_distance", 'rules' => 'required'),
 
-		);
-		$this->load->library('form_validation', $validation);
-		
-		if ($this->form_validation->run()) {
-			if ($this->routes_model->add_route($post)) {
-				$resp['code'] = 1;
-				$resp["msg"] = "route added sucessfully!";
-			}else{
-				$resp['code'] = 0;
-				$resp["msg"] = "Failed to add !";
-			}
-		} else {
-			$resp['code'] = 0;
-			$resp["msg"] = validation_errors();
-		}
+        );
+        $this->load->library('form_validation', $validation);
 
-		echo json_encode($resp);
-	}
+        if ($this->form_validation->run()) {
+            if ($this->routes_model->add_route($post)) {
+                $resp['code'] = 1;
+                $resp["msg"] = "route added sucessfully!";
+            } else {
+                $resp['code'] = 0;
+                $resp["msg"] = "Failed to add !";
+            }
+        } else {
+            $resp['code'] = 0;
+            $resp["msg"] = validation_errors();
+        }
 
+        echo json_encode($resp);
+    }
 
-	public function fetch_update_freight_form(){
-		$route_id = $this->input->get('route_id');
+    public function fetch_update_freight_form()
+    {
+        $route_id = $this->input->get('route_id');
 
-		$route_info = $this->routes_model->single_route($route_id);
+        $route_info = $this->routes_model->single_route($route_id);
 
-		foreach ($route_info as  $value) {
-			echo'
+        foreach ($route_info as $value) {
+            echo '
 					<form action="javascript:void();" method="Post" id="update_route_submit" accept-charset="utf-8">
 				<div class="panel">
 				<div class="panel-heading bg-primary">
@@ -79,8 +69,8 @@ class Route extends CI_Controller {
 				</div>
 				<div class="panel-body">
 					<div class="row">
-						
-					<input type="hidden" name="freight_id" value="'.$value->freight_id.'">
+
+					<input type="hidden" name="freight_id" value="' . $value->freight_id . '">
 						<div class="col-md-2">
 							<div class="form-group">
 								<label for="route">Route</label>
@@ -88,8 +78,8 @@ class Route extends CI_Controller {
 						</div>
 						<div class="col-md-4">
 							<div class="form-group">
-						<input type="text" name="route_name" value="'.$value->route_origin.' to '.$value->route_destination.'" class="form-control"   autofocus="autofocus" disabled>
-								<input type="hidden" name="route_id" id="Route_id" class="form-control" value="'.$value->route_id.'">
+						<input type="text" name="route_name" value="' . $value->route_origin . ' to ' . $value->route_destination . '" class="form-control"   autofocus="autofocus" disabled>
+								<input type="hidden" name="route_id" id="Route_id" class="form-control" value="' . $value->route_id . '">
 							</div>
 						</div>
 						<div class="col-md-2">
@@ -99,18 +89,18 @@ class Route extends CI_Controller {
 						</div>
 						<div class="col-md-4">
 							<div class="form-group">
-								<input type="date" name="freight_affect_date" value="'.$value->freight_affect_date.'" class="form-control"   autofocus="autofocus">
+								<input type="date" name="freight_affect_date" value="' . $value->freight_affect_date . '" class="form-control"   autofocus="autofocus">
 
 							</div>
 						</div>
-						
-						
-						
+
+
+
 					</div>
-					
-					
-					
-					
+
+
+
+
 					<div class="row">
 					<div class="col-md-2">
 							<div class="form-group">
@@ -120,10 +110,10 @@ class Route extends CI_Controller {
 						<div class="col-md-4">
 							<div class="form-group">
 								<input type="text" name="freight_rate" value=
-								"'.$value->freight_rate.'" class="form-control"   autofocus="autofocus">
+								"' . $value->freight_rate . '" class="form-control"   autofocus="autofocus">
 							</div>
 						</div>
-						
+
 						<div class="col-md-2">
 							<div class="form-group">
 								<label for="CMVR">CMVR</label>
@@ -131,68 +121,64 @@ class Route extends CI_Controller {
 						</div>
 						<div class="col-md-4">
 							<div class="form-group">
-								<input type="text" name="freight_cmvr" value="'.$value->freight_cmvr.'" class="form-control"   autofocus="autofocus">
+								<input type="text" name="freight_cmvr" value="' . $value->freight_cmvr . '" class="form-control"   autofocus="autofocus">
 							</div>
 						</div>
-						
+
 					</div>
-					
+
 					<div class="row">
-						
+
 						<div class="col-md-3 col-md-offset-9">
 							<button type="button" class="btn btn-danger" data-dismiss="modal">close</button>
 							<button type="submit" class="btn btn-success" id="personal_information">Update</button>
 						</div>
 					</div>
-					
+
 				</div>
-				
+
 			</div>
 		</form>
 
 			';
-		}
+        }
 
-	}
+    }
 
-	public function update_freight(){
-		$post = $this->input->post();
-		
-		$validation = array(
-			
-			
-			array('field'=>"route_id", 'rules'=>'required'),
-			array('field'=>"freight_rate", 'rules'=>'required'),
-			array('field'=>"freight_cmvr", 'rules'=>'required'),
-			array('field'=>"freight_affect_date", 'rules'=>'required')
+    public function update_freight()
+    {
+        $post = $this->input->post();
 
-		);
-		$this->load->library('form_validation', $validation);
-		
-		if ($this->form_validation->run()) {
-			if ($this->routes_model->update_freight($post)) {
-				$resp['code'] = 1;
-				$resp["msg"] = "freight updated sucessfully!";
-			}else{
-				$resp['code'] = 0;
-				$resp["msg"] = "Failed to add !";
-			}
-		} else {
-			$resp['code'] = 0;
-			$resp["msg"] = validation_errors();
-		}
+        $validation = array(
 
-		echo json_encode($resp);
-	}
+            array('field' => "route_id", 'rules' => 'required'),
+            array('field' => "freight_rate", 'rules' => 'required'),
+            array('field' => "freight_cmvr", 'rules' => 'required'),
+            array('field' => "freight_affect_date", 'rules' => 'required'),
 
+        );
+        $this->load->library('form_validation', $validation);
 
+        if ($this->form_validation->run()) {
+            if ($this->routes_model->update_freight($post)) {
+                $resp['code'] = 1;
+                $resp["msg"] = "freight updated sucessfully!";
+            } else {
+                $resp['code'] = 0;
+                $resp["msg"] = "Failed to add !";
+            }
+        } else {
+            $resp['code'] = 0;
+            $resp["msg"] = validation_errors();
+        }
 
+        echo json_encode($resp);
+    }
 
-	public function __construct()
-	{
-		parent:: __construct();
-		$this->load->model('routes_model');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('routes_model');
+    }
 
 }
-
--- assets/js/custom/route.js
+++ assets/js/custom/route.js
@@ -1,119 +1,96 @@
 var url = $("#url").val();
-
-
-
-    jQuery(document).ready(function($) {
-        $("#loadingBlock").hide();
-        load_route_data();
-    });
-
-function load_route_data(){
-	 $("#loadingBlock").show();
-            $.get(url+'route/', function(data) {
-        $("#route_info").html(data);
-        $("#loadingBlock").hide();
-    });
-}
-
-$("#add_route").click(function(event) {
-    $.ajax({
-        url: url + "operations/add_route",
-        method: "POST",
-        success: function(data) {
-            $('#route').html(data);
-        }
-    })
+jQuery(document).ready(function ($) {
+    $("#dataTable").dataTable();
+    $("#loadingBlock").hide();
 });
 
-$(document).on('click',"#delete_route_btn",function(event) {
+$("#add_new_route").click(function (e) { 
+    e.preventDefault();
+    $.get(url+"route/add_route_form",function (data){
+        $('.modal-body').html(data);
+        $("#modal-id").modal('show');
+    });
+});
+
+$(document).on('click', "#delete_route_btn", function (event) {
     var conf = confirm("Do you really want to delete this route ??");
     if (conf == true) {
-	   var route_id = $(this).attr('value');
-        $.get(url+'route/delete_route?route_id='+route_id, function(data) {
+        var route_id = $(this).attr('value');
+        $.get(url + 'route/delete_route?route_id=' + route_id, function (data) {
             if (data == "1") {
                 location.reload();
-            }else{
+            } else {
                 alert("failed to delete route");
             }
         });
     }
-   
+
 });
 
-// $(document).on('change','#client_id',function(event){
-// 	  var client_id = $('#client_id').val();
-     
-// 	   $.get(url+'route?client_id='+client_id, function(data) {
-//                 $("#route_info").html(data);
-//                 $("#loadingBlock").hide();
-//             });
-// 	});
+$(document).on('change', '#consignment_type', function (event) {
+    var consignment_type = $('#consignment_type').val();
+    if (consignment_type == 'market') {
+        $('#freight').hide();
+        $('#cmvr').hide();
+    } else {
+        $('#freight').show();
+        $('#cmvr').show();
+    }
 
+});
 
-$(document).on('change','#consignment_type',function(event){
-      var consignment_type = $('#consignment_type').val();
-       if(consignment_type == 'market'){
-            $('#freight').hide();
-            $('#cmvr').hide();
-        }else{
-            $('#freight').show();
-            $('#cmvr').show();
+$(document).on('submit', '#add_route_submit', function (event) {
+
+    event.preventDefault();
+    $.ajax({
+        url: url + 'route/add_route',
+        type: 'POST',
+        data: new FormData(this),
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            var resp = jQuery.parseJSON(data);
+            console.log(data);
+            if (resp.code == 1) {
+                alert(resp.msg);
+                location.reload(true);
+                // $("#add_route_submit").trigger("reset");
+
+            } else {
+                alert(resp.msg);
+
+            }
+
+            $("#loadingBlock").hide();
+
         }
-        
-    });
+    })
+});
+$(document).on('submit', '#update_route_submit', function (event) {
 
-$(document).on('submit', '#add_route_submit', function(event) {
-	
-            event.preventDefault();
-            $.ajax({
-                url: url+'route/add_route',
-                type: 'POST',
-                data: new FormData(this),
-                cache:false,
-                contentType:false,
-                processData:false,
-                success:function(data){
-                        var resp = jQuery.parseJSON(data);
-                        console.log(data);
-                        if (resp.code == 1) {
-                           alert(resp.msg);
-                           location.reload(true);
-                           // $("#add_route_submit").trigger("reset");
+    event.preventDefault();
+    $.ajax({
+        url: url + 'route/update_freight',
+        type: 'POST',
+        data: new FormData(this),
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
 
-                        }else{
-                            alert(resp.msg);
-                            
-                        }
+            var resp = jQuery.parseJSON(data);
+            console.log(data);
+            if (resp.code == 1) {
+                $('#modal-id').modal('hide');
+                alert(resp.msg);
+            } else {
+                alert(resp.msg);
 
-                        $("#loadingBlock").hide();
+            }
 
-                }
-            })
-        });
-$(document).on('submit', '#update_route_submit', function(event) {
-	
-            event.preventDefault();
-            $.ajax({
-                url: url+'route/update_freight',
-                type: 'POST',
-                data: new FormData(this),
-                cache:false,
-                contentType:false,
-                processData:false,
-                success:function(data){
+            $("#loadingBlock").hide();
 
-                        var resp = jQuery.parseJSON(data);
-                        console.log(data);
-                        if (resp.code == 1) {
-                           $('#modal-id').modal('hide');
-                           alert(resp.msg);
-                        }else{
-                            alert(resp.msg);
-                            
-                        }
-
-                        $("#loadingBlock").hide();
-
-                }
-            })
-        });
+        }
+    })
+});
