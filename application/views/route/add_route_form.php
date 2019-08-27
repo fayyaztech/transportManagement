43acd5717e368f1f6ac88a0b13a4399bce40b9c9
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-08-27 11:44:16 +0530

    form new route

---
+++ application/views/route/add_route_form.php
@@ -0,0 +1,59 @@
+<form action="javascript:void();" method="Post" id="add_route_submit"accept-charset="utf-8">
+                            <div class="panel">
+                                <div class="panel-heading bg-primary">
+                                    <h3 class="text-center">Add Route</h3>
+                                </div>
+                                <div class="panel-body">
+                                    
+                                   
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label for="origin">Origin</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <input type="text" name="route_origin" class="form-control" autofocus="autofocus">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label for="destination">Destination</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <input type="text" name="route_destination" class="form-control"   autofocus="autofocus">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label for="Distance">Distance</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <input type="text" name="route_distance" class="form-control"  autofocus="autofocus">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                    
+                                    <div class="row">
+                                        
+                                        <div class="col-md-3 col-md-offset-9">
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">close</button>
+                                            <button type="submit" class="btn btn-success" id="personal_information">Submit</button>
+                                            <button type="button" class="hidden reset-btn">Custom Reset Button</button>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                        </form>
