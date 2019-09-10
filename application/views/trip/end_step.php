7c3929815ad4b39ac3358e104b1a1e9fc487c4c6
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-09-10 15:12:57 +0530

    database field changed

--- application/views/trip/end_step.php
+++ application/views/trip/end_step.php
@@ -11,7 +11,7 @@
 			<div class="row">
 			<div class="form-group col-md-12">
 				<label for="">End date</label>
-				<input type="date" class="form-control col-md-6" name="trip_stop_date" placeholder="dd/mm/yyyy" required="">
+				<input type="date" class="form-control col-md-6" name="step_end_date" placeholder="dd/mm/yyyy" required="">
 			</div>
 			<div class="form-group col-md-12">
 				<label for="">Note</label>
