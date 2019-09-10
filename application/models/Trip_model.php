2b10b76d9804255b4b66bd9eddf2b9738b24a876
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-09-10 15:27:55 +0530

    functions are commited whose reference was not found.

--- application/models/Trip_model.php
+++ application/models/Trip_model.php
@@ -5,21 +5,21 @@ class Trip_model extends CI_Model
 {
     public function fetch_step_details($step_id)
     {
-        $this->db->select('td.trip_details_id,td.trip_id,td.load_id,td.driver_id,td.route_id,td.trip_start_date,td.trip_detail_freight');
+        $this->db->select('td.trip_details_id,td.trip_id,td.load_id,td.driver_id,td.route_id,td.step_start_date,td.trip_detail_freight');
         $this->db->where('trip_details_id', $step_id);
         return $this->db->get('trip_details as td')->row_array();
     }
-    public function get_incomplete_trips($trip_id)
-    {
-        $this->db->select('trip_details_id,driver_id');
-        $this->db->where('trip_id', $trip_id);
-        $this->db->where('trip_detail_status', 2);
-        return $this->db->get("trip_details")->result();
-    }
+    // public function get_incomplete_trips($trip_id)
+    // {
+    //     $this->db->select('trip_details_id,driver_id');
+    //     $this->db->where('trip_id', $trip_id);
+    //     $this->db->where('trip_detail_status', 2);
+    //     return $this->db->get("trip_details")->result();
+    // }
 
     public function fetch_trip_details($trip_id)
     {
-        $this->db->select('td.trip_id,td.route_id,td.driver_id,t.consignor_id');
+        $this->db->select('td.trip_id,td.route_id,td.driver_id,t.consignor_id,allowance,t.vehicle_id');
         $this->db->where('td.trip_id', $trip_id);
         $this->db->order_by('td.trip_details_id', 'desc');
         $this->db->join('trip as t', 't.trip_id = td.trip_id', 'left');
@@ -32,7 +32,7 @@ class Trip_model extends CI_Model
         $this->db->where('trip_details_id', $data['trip_details_id']);
         $data['trip_detail_status'] = 3;
         unset($data['trip_details_id']);
-        if ($this->db->update('trip_details',$data)) {
+        if ($this->db->update('trip_details', $data)) {
             $r = true;
         }
 
@@ -47,7 +47,7 @@ class Trip_model extends CI_Model
             $this->db->insert(
                 'trip',
                 [
-                    'trip_start_date' => $data['trip_start_date'],
+                    'step_start_date' => $data['step_start_date'],
                     'vehicle_id' => $data['vehicle_id'],
                     'consignor_id' => $data['consignor_id'],
                     'consignee_id' => $data['consignee_id'],
@@ -77,15 +77,15 @@ class Trip_model extends CI_Model
         // $status if 1 active vehicle if 0 inactive vehicle
         $this->db->select('trip.*,trip_details.*');
         $this->db->select('route.route_id,route.route_origin,route.route_destination,route.route_distance');
-        $this->db->select('loads.load_name,consignee_name,trip_stop_date');
+        $this->db->select('loads.load_name,consignee_name,step_end_date');
         $this->db->select('driver.driver_name');
         $this->db->select('consignors.consignor_name');
         $this->db->select('vehicle.vehicle_id,vehicle.vehicle_number');
-        // $this->db->order_by('trip.trip_start_date', 'desc');
+        // $this->db->order_by('trip.step_start_date', 'desc');
         $this->db->join('trip_details', 'trip.trip_id = trip_details.trip_id', 'left');
         $this->db->join('routes as route', 'trip_details.route_id = route.route_id', 'left');
-        $this->db->join('consignors', 'trip.consignor_id =consignors.consignor_id');
-        $this->db->join('vehicle', 'trip.vehicle_id= vehicle.vehicle_id', 'left');
+        $this->db->join('consignors', 'trip.consignor_id = consignors.consignor_id');
+        $this->db->join('vehicle', 'trip.vehicle_id = vehicle.vehicle_id', 'left');
         $this->db->join("loads", 'trip_details.load_id = loads.load_id', 'left');
         $this->db->join('driver', 'trip_details.driver_id = driver.driver_id');
         $this->db->join('consignees', 'trip.consignee_id = consignees.consignee_id', 'left');
@@ -110,25 +110,22 @@ class Trip_model extends CI_Model
 
     public function update_trip_info($info)
     {
-        //print_r($info);
+        $r = false;
         $trip_id = $info['trip_id'];
         unset($info['trip_id']);
         $this->db->where('trip_id', $trip_id);
-        if ($this->db->set(['driver_id' => $info['driver_id'], 'route_id' => $info['route_id']])) {
-            $this->db->update('trip_details');
-        }
-        if ($this->db->set(['allowance' => $info['allowance']])) {
-            $this->db->update('trip');
+        if ($this->db->update('trip', $info)) {
+            $r = true;
         }
 
-        return true;
+        return $r;
     }
 
 // // Stop Trip Function
 
     public function stop_trip($data)
     {
-        $stop_date = $data['trip_stop_date'];
+        $stop_date = $data['step_end_date'];
         $trip_id = $data['trip_id'];
         unset($data['trip_id']);
         $this->db->where('trip_id', $trip_id);
@@ -140,33 +137,33 @@ class Trip_model extends CI_Model
 
 // // Fetch Trip Data For Update
 
-    public function fetch_trip_info($trip_id)
-    {
-        $this->db->select('trip.*');
-        //
-        $this->db->select('routes.route_id,routes.route_origin,routes.route_destination,routes.route_distance');
-        $this->db->select('trip_details.route_id,trip_details.driver_id,trip_details.trip_id,trip_details.trip_start_date,trip_details.trip_detail_status');
+    // public function fetch_trip_info($trip_id)
+    // {
+    //     $this->db->select('trip.*');
+    //     //
+    //     $this->db->select('routes.route_id,routes.route_origin,routes.route_destination,routes.route_distance');
+    //     $this->db->select('trip_details.route_id,trip_details.driver_id,trip_details.trip_id,trip_details.step_start_date,trip_details.trip_detail_status');
 
-        $this->db->select('trip.*');
-        //
-        $this->db->select('routes.route_id,routes.route_origin,routes.route_destination,routes.route_distance');
-        $this->db->select('load_type.load_name,trip_details.route_id,trip_details.trip_id,trip_details.trip_start_date,trip_details.trip_detail_status');
-        $this->db->select('driver.driver_name');
-        $this->db->select('consignors.consignor_name');
-        $this->db->select('vehicle.vehicle_id,vehicle.vehicle_number');
-        $this->db->join('trip_details', 'trip.trip_id = trip_details.trip_id', 'left');
-        $this->db->join('routes', 'trip_details.route_id = routes.route_id', 'left');
-        $this->db->join('consignors', 'trip.consignor_id = consignors.consignor_id');
-        $this->db->join('vehicle', 'trip.vehicle_id= vehicle.vehicle_id', 'left');
-        $this->db->join("load_type", 'trip_details.load_id = load_type.load_id', 'left');
-        $this->db->join('driver', 'trip_details.driver_id = driver.driver_id');
-        $this->db->from('trip');
-        $query = $this->db->get();
-        return $query->result();
+    //     $this->db->select('trip.*');
+    //     //
+    //     $this->db->select('routes.route_id,routes.route_origin,routes.route_destination,routes.route_distance');
+    //     $this->db->select('load_type.load_name,trip_details.route_id,trip_details.trip_id,trip_details.step_start_date,trip_details.trip_detail_status');
+    //     $this->db->select('driver.driver_name');
+    //     $this->db->select('consignors.consignor_name');
+    //     $this->db->select('vehicle.vehicle_id,vehicle.vehicle_number');
+    //     $this->db->join('trip_details', 'trip.trip_id = trip_details.trip_id', 'left');
+    //     $this->db->join('routes', 'trip_details.route_id = routes.route_id', 'left');
+    //     $this->db->join('consignors', 'trip.consignor_id = consignors.consignor_id');
+    //     $this->db->join('vehicle', 'trip.vehicle_id= vehicle.vehicle_id', 'left');
+    //     $this->db->join("load_type", 'trip_details.load_id = load_type.load_id', 'left');
+    //     $this->db->join('driver', 'trip_details.driver_id = driver.driver_id');
+    //     $this->db->from('trip');
+    //     $query = $this->db->get();
+    //     return $query->result();
 
-        $query = $this->db->get();
-        return $query->result();
-    }
+    //     $query = $this->db->get();
+    //     return $query->result();
+    // }
 
     public function update_step($data)
     {
