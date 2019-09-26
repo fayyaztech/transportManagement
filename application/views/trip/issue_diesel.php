ed79157c43ce4bf145026d13cb62f80b67994bd1
Faiyaz Shaikh <fayyaztech@gmail.com>
2019-09-26 14:55:22 +0530

    issue_diesel

--- application/views/trip/issue_diesel.php
+++ application/views/trip/issue_diesel.php
@@ -5,5 +5,18 @@
       </div>
       <div class="panel-body">
             
+            <form action="javaScript(void());" method="POST" role="form">
+                <legend>Form title</legend>
+            
+                <div class="form-group">
+                    <label for="">label</label>
+                    <input type="text" class="form-control" id="" placeholder="Input field">
+                </div>
+            
+                
+            
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            
       </div>
 </div>
